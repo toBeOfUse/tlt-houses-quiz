31c7bc44fe07b37bdee13bbb4c0e7b47fdfbb347
Mitch J <mjacovet@kent.edu>
2025-07-19 08:21:34 -0400

    reduce number of js build files

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()]
+	plugins: [sveltekit(), devtoolsJson()],
+	// https://github.com/sveltejs/kit/issues/3882#issuecomment-2294459036
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: (id) => {
+					return 'my-app';
+				}
+			}
+		}
+	}
 });
